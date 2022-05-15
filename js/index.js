bf49cf50d63d3cf21d859e92887dd69f72fbdaec
Алан Богов <avavionmvm@gmail.com>
2022-05-15 03:08:50 +0300

    Initital Commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const dropdown = (selector) => {
+    const parent = document.querySelector(selector);
+
+    if (!parent) return false;
+
+
+}
+
+const init = () => {
+    console.log('init();');
+
+    // dropdown();
+}
+
+document.addEventListener('DOMContentLoaded', init);
