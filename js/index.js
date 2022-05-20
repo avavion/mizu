fe6683e9aaf3b549ed26e41306399ef87ef503b5
Алан Богов <avavionmvm@gmail.com>
2022-05-20 03:01:19 +0300

    Add: slider, faq, mobile styles

--- js/index.js
+++ js/index.js
@@ -2,21 +2,18 @@ const scrollOff = () => document.body.style.overflow = 'hidden';
 const scrollOn = () => document.body.style.overflow = '';
 
 const dropdownInit = () => {
-    const dropdowns = document.querySelectorAll('.dropdown');
+    const items = document.querySelectorAll('[data-faq-item]');
 
-    if (dropdowns.length) return false;
+    if (!items.length) return false;
 
-    dropdowns.forEach((dropdown) => {
-        const items = dropdown.querySelector('.dropdown-item');
 
-        items.forEach((item) => {
-            item.addEventListener('click', ({ currentTarget }) => {
-                if (currentTarget.classList.contains('active')) {
-                    currentTarget.classList.remove('active');
-                } else {
-                    currentTarget.classList.add('active');
-                }
-            })
+    items.forEach((item) => {
+        item.addEventListener('click', ({ currentTarget }) => {
+            if (currentTarget.classList.contains('active')) {
+                currentTarget.classList.remove('active');
+            } else {
+                currentTarget.classList.add('active');
+            }
         })
     });
 }
@@ -49,11 +46,46 @@ const burgerMenu = () => {
     })
 }
 
+const slider = (selector) => {
+    const parent = document.querySelector(selector);
+
+    if (!parent) return false;
+
+    const container = parent.querySelector('.swiper');
+
+    const timeline = parent.querySelector('.timeline');
+    const items = timeline.querySelectorAll('.item');
+
+    const clear = () => {
+        for (const item of items) {
+            item.classList.remove('active');
+        }
+    }
+
+    const setActive = (index) => {
+        clear();
+        items[index].classList.add('active');
+    }
+
+    const swiper = new Swiper(container, {
+        autoHeight: true,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 120,
+        autoplay: {
+            delay: 5000
+        },
+        on: {
+            init: ({ activeIndex }) => setActive(activeIndex),
+            slideChange: ({ activeIndex }) => setActive(activeIndex)
+        }
+    });
+}
+
 const init = () => {
-    console.log('init();');
+    slider('.section-slider');
     burgerMenu();
     dropdownInit();
-    // dropdown();
 }
 
 document.addEventListener('DOMContentLoaded', init);
